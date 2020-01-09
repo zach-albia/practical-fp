8b5113b7e6922628d3b0485c7cf4462a34b8108d
Zach Albia <zach.albia@gmail.com>
2020-01-09 21:40:26 +0800

    feat: implement topN

--- src/main/java/practical/fp/TopN.java
+++ src/main/java/practical/fp/TopN.java
@@ -1,7 +1,7 @@
 package practical.fp;
 
 import java.io.File;
-import java.util.Scanner;
+import java.util.*;
 
 public class TopN {
     public static void main(String[] args) {
@@ -10,14 +10,39 @@ public class TopN {
         String input = sc.nextLine();
         System.out.print("Enter number of files: ");
         int n = sc.nextInt();
-        File[] files = topN(new File(input), n);
+        Iterable<File> files = topN(new File(input), n);
         for (File file : files) {
-            System.out.println(file.getName());
+            System.out.println(file.getAbsolutePath());
         }
         sc.close();
     }
 
-    private static File[] topN(File file, int n) {
-        return null;
+    private static Comparator<File> compareLengthDescending =
+            Comparator.comparingLong(File::length);
+
+    private static Iterable<File> topN(File path, int n) {
+        if (path.exists()) {
+            if (path.isFile()) {
+                return Collections.singleton(path);
+            } else { // if directory
+                PriorityQueue<File> topNFiles = new PriorityQueue<>(n + 1, compareLengthDescending);
+                doTopN(path, n, topNFiles);
+                return topNFiles;
+            }
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    private static void doTopN(File path, int n, PriorityQueue<File> topNFiles) {
+        File[] files = path.listFiles();
+        for (File path_ : files) {
+            if (path_.isFile()) {
+                topNFiles.add(path_);
+                if (topNFiles.size() > n) topNFiles.remove();
+            } else {
+                doTopN(path_, n, topNFiles);
+            }
+        }
     }
 }
